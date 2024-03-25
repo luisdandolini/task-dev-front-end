a1b15c82b38a084da9e3d1535be300cf71d3863c
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-03-24 21:00:12 -0300

    feat: created method post and get tasks

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,16 +1,58 @@
 import styles from './Content.module.css';
 import { PlusCircle } from "@phosphor-icons/react";
 import { ClipboardText } from '@phosphor-icons/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Task } from '../Task/Task';
+
+interface Task {
+  id: number;
+  name: string;
+}
 
 export function Content(){
+  const [taskName, setTaskName] = useState('');
+  const [allTasks, setAllTasks] = useState<Task[]>([]);
+
+  const handleTaskName = (event: any) => {
+    setTaskName(event.target.value);
+  };
+
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+
+  const createdTask = () => {
+    axios.post('http://localhost:3000/tasks', {
+      name: taskName
+    }).then(response => {
+      console.log(response.data);
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+  const getAllTasks = () => {
+    axios.get('http://localhost:3000/tasks')
+      .then(response => {
+        console.log(response.data);
+        setAllTasks(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
   return(
     <main>
       <div className={styles.created}>
         <input 
           type="text" 
           placeholder="Adicione uma nova tarefa"
+          value={taskName}
+          onChange={handleTaskName}
         />
-        <button>
+        <button onClick={createdTask}>
           Criar
           <PlusCircle />
         </button>
@@ -31,11 +73,23 @@ export function Content(){
       <div className={styles.divider}></div>
 
       <div className={styles.container_content}>
-        <div className={styles.content}>
-          <ClipboardText size={52} color='#808080'/>
-          <p>Você ainda não tem tarefas cadastradas</p>
-          <p>Crie tarefas e organize seus itens a fazer</p>
-        </div>
+        {
+          allTasks.length <= 0 ? (
+            <div className={styles.content}>
+              <ClipboardText size={52} color='#808080'/>
+              <p>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          ) : (
+            <div>
+              {allTasks.map(task => (
+                <div key={task.id}>
+                  <Task content={task.name} />
+                </div>
+              ))}
+            </div>
+          )
+        }
       </div>
     </main>
   )
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,7 +1,26 @@
-export function Task() {
+import styles from './Task.module.css';
+import { Trash } from "@phosphor-icons/react"
+
+interface Props {
+  content: string
+}
+
+export function Task({ content }: Props) {
   return(
-    <div>
-      
+    <div className={styles.container}>
+      <input 
+        type="checkbox" 
+        className={styles.checkbox}
+      />
+      <label className={styles.custom_checkbox}></label>
+
+      <p>
+        {content}
+      </p>
+
+      <button>
+        <Trash size={20} color='#808080'/>
+      </button>
     </div>
   )
-}
+} 
